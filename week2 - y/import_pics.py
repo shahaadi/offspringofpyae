be7edc413bd0171a011ecda6a51086c60590574b
thenerd31 <aswinsurya@gmail.com>
2023-07-21 08:26:11 -0700

    added working gui

--- week2 - y/import_pics.py
+++ week2 - y/import_pics.py
@@ -4,26 +4,22 @@ from database import Database
 from model import Model
 from model import display_faces
 
-database = Database()
-database_filename = input("Enter the database filename (e.g., something.pkl): ")
-if database_filename:
-    database.load_db(database_filename)
+def load_database(filename):
+    database = Database()
+    if filename:
+        database.load_db(filename)
+    return database
 
-while input("Enter 'n' to terminate your session and save your database: ") != 'n':
-    path_to_image = input("Enter the path to the image: ")
-    name_image = input("Enter name of person in image: ")
-
-    # shape-(Height, Width, Color)
-    image = io.imread(str(path_to_image))
+def recognize_and_display_faces(image_path, database, cos_dist_threshold=0.6):
+    image = io.imread(str(image_path))
     if image.shape[-1] == 4:
         # Image is RGBA, where A is alpha -> transparency
         # Must make image RGB.
         image = image[..., :-1]  # png -> RGB
 
-    recognized_faces, valid_descriptors = Model.recognize_faces(image, database, cos_dist_threshold=0.6)
+    recognized_faces, valid_descriptors = Model.recognize_faces(image, database, cos_dist_threshold)
     display_faces(image, recognized_faces)
-    if input(f"Enter 'y' to add {name_image} to the database at the box you see: ") == 'y':
-        database.add_profile(name_image, valid_descriptors[0])
+    return recognized_faces, valid_descriptors
 
-database_filename = input("Enter the database filename (e.g., something.pkl): ")
-database.save_db(database_filename)
+def add_to_database(name, descriptor, database):
+    database.add_profile(name, descriptor)
--- week2 - y/main.py
+++ week2 - y/main.py
@@ -3,31 +3,60 @@ from database import Database
 
 from model import Model
 from model import display_faces
+import tkinter as tk
+from tkinter import filedialog
+import import_pics 
 
-# database_filename = input("Enter the database filename (e.g., something.pkl): ")
-# database = db.load_db(database_filename)
-database = Database()
+class FaceRecognitionApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Face Recognition App")
+        self.database = import_pics.load_database(input("Enter the database filename (e.g., something.pkl): "))
+        self.create_widgets()
 
-path_to_image = input("Enter the path to the image: ")
+    def create_widgets(self):
+        self.label_filename = tk.Label(self, text="No image selected.")
+        self.label_filename.pack()
 
-# shape-(Height, Width, Color)
-image = io.imread(str(path_to_image))
-if image.shape[-1] == 4:
-    # Image is RGBA, where A is alpha -> transparency
-    # Must make image RGB.
-    image = image[..., :-1]  # png -> RGB
+        self.select_button = tk.Button(self, text="Select Image", command=self.select_image)
+        self.select_button.pack()
 
-recognized_faces, _ = Model.recognize_faces(image, database)
-display_faces(image, recognized_faces)
+        self.recognize_button = tk.Button(self, text="Recognize Faces", command=self.recognize_faces)
+        self.recognize_button.pack()
 
-unknown_faces = [face for face in recognized_faces if face[0] == "Unknown"]
-# Add the unknown faces to the database
-for i, (name, _, _) in enumerate(unknown_faces, start=1):
-    add_to_db = input(f"Add unknown face {i} to the database? (y/n): ")
-    if add_to_db.lower() == "y":
-        name = input(f"Enter a name for unknown face {i}: ")
-        descriptor = [face[1] for face in recognized_faces if face[0] == "Unknown"][i-1]
-        database.add_profile(name, descriptor)
+        self.add_to_db_button = tk.Button(self, text="Add to Database", command=self.add_to_database)
+        self.add_to_db_button.pack()
 
-print("Database after adding unknown faces:")
-database.display_database()
+        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
+        self.quit_button.pack()
+
+    def select_image(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.png;*.jpeg")])
+        if file_path:
+            self.image_path = file_path
+            self.label_filename.config(text=f"Selected Image: {file_path}")
+        else:
+            self.label_filename.config(text="No image selected.")
+
+    def recognize_faces(self):
+        if hasattr(self, "image_path"):
+            recognized_faces, valid_descriptors = import_pics.recognize_and_display_faces(self.image_path, self.database)
+            self.valid_descriptors = valid_descriptors
+
+    def add_to_database(self):
+        if hasattr(self, "valid_descriptors"):
+            name = input("Enter name of person in image: ")
+            import_pics.add_to_database(name, self.valid_descriptors[0], self.database)
+            print(f"{name} has been added to the database.")
+
+    def quit(self):
+        # Save the database and close the GUI
+        database_filename = input("Enter the database filename (e.g., something.pkl): ")
+        if database_filename:
+            self.database.save_db(database_filename)
+            print("Database saved.")
+        super().quit()
+
+if __name__ == "__main__":
+    app = FaceRecognitionApp()
+    app.mainloop()
