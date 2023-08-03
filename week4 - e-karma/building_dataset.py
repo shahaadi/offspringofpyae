2e5e0bd1668b2bf2e146bd89a9371dc5efcc7776
shahaadi <42944554+shahaadi@users.noreply.github.com>
2023-08-03 09:22:07 -0700

    Created new dataset files

--- week4 - e-karma/building_dataset.py
+++ week4 - e-karma/building_dataset.py
@@ -1,80 +1,91 @@
-import requests, json
 import numpy as np
 import pandas as pd
+import requests, json
+from yt_dlp import YoutubeDL
 from tqdm import tqdm
-def getBestTimeStamps(datapoints: np.ndarray, cutoff: float = 0.8):
-    new_start = np.argmax((datapoints[1:] - datapoints[:-1]) > 0)
-    datapoints[:new_start] = 0
-    datapoints *= 1 / np.max(datapoints)
+import os
 
-    peak = np.argmax(datapoints)
+# pulls song ids and titles
+def save_pre_data(channel_id, path):
+    SEP = ';'
 
-    good_dp = datapoints > cutoff
-    prev_dp = ~good_dp[:peak][::-1]
-    if prev_dp.any():
-        start_idx = peak - np.argmax(prev_dp)
-    else:
-        start_idx = 0
+    try:
+        os.remove(path)
+    except OSError:
+        pass
 
-    post_dp = ~good_dp[peak:]
-    if post_dp.any():
-        end_idx = peak + np.argmax(post_dp)
-    else:
-        end_idx = good_dp.shape[0]
+    basecommand = f'python -m yt_dlp --flat-playlist -j --print-to-file "{channel_id};%(id)s;%(title)s;;" "{path}" "https://www.youtube.com/channel/{channel_id}"'
+    os.system(basecommand)
 
-    start_ms = start_idx * t_step
-    end_ms = end_idx * t_step
+    song_data = pd.read_csv(path, sep=SEP, header=None)
+    columns = ['channel_id', 'video_id', 'title', 'duration', 'view_count']
+    song_data.columns = columns
 
-    return (start_ms, end_ms)
+    song_data[[f'datapoint_{i}' for i in range(100)]] = np.nan
+    song_data.to_csv(path, sep=SEP)
 
-song_data = pd.read_csv('video_data.csv')
-print(song_data)
+# pull song data and save to path
+def save_song_data(path):
+    SEP = ';'
+    song_data = pd.read_csv(path, sep=SEP)
 
-for index in tqdm(range(len(song_data['video_id']) - 1, -1, -1)):
-    videoID = song_data.at[index, 'video_id']
-    if song_data.loc[[index]].isna().sum().sum() > 1:
-        # get video activity from video
-        url = f'https://yt.lemnoslife.com/videos?part=mostReplayed&id={videoID}'
-        content = requests.get(url).text
-        data = json.loads(content)
-        datapoints = np.empty((100,))
-        print(videoID)
-        if data['items'][0]['mostReplayed'] is None:
-            song_data = song_data.drop(index)
-        else:
-            # get song_duration
-            url = f'https://yt.lemnoslife.com/videos?part=contentDetails&id={videoID}'
+    for index in tqdm(range(len(song_data['video_id']) - 1, -1, -1)):
+        videoID = song_data.at[index, 'video_id']
+        if song_data.loc[[index]].isna().sum().sum() != 0:
+            # get video activity from video
+            url = f'https://yt.lemnoslife.com/videos?part=mostReplayed&id={videoID}'
             content = requests.get(url).text
-            time_data = json.loads(content)
-            duration = time_data['items'][0]['contentDetails']['duration']
-            if duration > 600:
+            data = json.loads(content)
+            print(videoID)
+            if data['items'][0]['mostReplayed'] is None:
                 song_data = song_data.drop(index)
             else:
-                t_step = data['items'][0]['mostReplayed']['heatMarkers'][0]['heatMarkerRenderer']['markerDurationMillis']
-                for i, heatMarker in enumerate(data['items'][0]['mostReplayed']['heatMarkers']):
-                    heatMarker = heatMarker['heatMarkerRenderer']
-                    intensityScoreNormalized = heatMarker['heatMarkerIntensityScoreNormalized']
-                    datapoints[i] = intensityScoreNormalized
-                song_data.at[index, 'optimal_start'], song_data.at[index, 'optimal_end'] = getBestTimeStamps(datapoints)
-                song_data.at[index, 'song_duration'] = duration
-
-                # get video_title
-                url = f'https://yt.lemnoslife.com/noKey/videos?part=snippet&id={videoID}'
+                # get song_duration
+                url = f'https://yt.lemnoslife.com/videos?part=contentDetails&id={videoID}'
                 content = requests.get(url).text
-                data = json.loads(content)
-                song_data.at[index, 'video_title'] = data['items'][0]['snippet']['title']
+                time_data = json.loads(content)
+                duration = time_data['items'][0]['contentDetails']['duration']
+                if duration > 600:
+                    song_data = song_data.drop(index)
+                else:
+                    t_step = data['items'][0]['mostReplayed']['heatMarkers'][0]['heatMarkerRenderer']['markerDurationMillis']
+                    for i, heatMarker in enumerate(data['items'][0]['mostReplayed']['heatMarkers']):
+                        song_data.at[index, f'datapoint_{i}'] = heatMarker['heatMarkerRenderer']['heatMarkerIntensityScoreNormalized']
+                    song_data.at[index, 'duration'] = duration
 
-                # get song_name
-                url = f'https://yt.lemnoslife.com/videos?part=musics&id={videoID}'
-                content = requests.get(url).text
-                data = json.loads(content)
-                if len(data['items'][0]['musics']) != 0:
-                    song_data.at[index, 'song_name'] = data['items'][0]['musics'][0]['song']['title']
+                    # get view_count of video
+                    url = f'https://yt.lemnoslife.com/noKey/videos?part=statistics&id={videoID}'
+                    content = requests.get(url).text
+                    data = json.loads(content)
+                    song_data.at[index, 'view_count'] = data['items'][0]['statistics']['viewCount']
+                    song_data.to_csv(path, sep=SEP)
 
-                # get viewcount of video
-                url = f'https://yt.lemnoslife.com/noKey/videos?part=statistics&id={videoID}'
-                content = requests.get(url).text
-                data = json.loads(content)
-                song_data.at[index, 'viewcount'] = data['items'][0]['statistics']['viewCount']
-        if index % 100 == 0:
-            song_data.to_csv('video_data.csv')
+# removes leading columns that are not channel_id
+def remove_irrev_cols(path):
+    song_data = pd.read_csv(path)
+    while song_data.columns[0] != 'channel_id':
+        song_data = song_data.drop(song_data.columns[0], axis=1)
+
+# cleans data by removing non songs
+def remove_non_songs(channel_id, path):
+    keyword = None
+    if channel_id == ''
+    if keyword is not None:
+        for index in range(len(song_data['video_id']) - 1, -1, -1):
+
+
+# all in one function
+def save_data(channel_id):
+    save_pre_data(channel_id, f'{channel_id}_output.csv')
+    save_song_data(f'{channel_id}_output.csv')
+    remove_irrev_cols(f'{channel_id}_output.csv')
+    remove_non_songs(channel_id, f'{channel_id}_output.csv')
+
+# merge all channel csvs
+def save_and_concat(channel_ids):
+    dfs = []
+    for channel_id in channel_ids:
+        save_data(channel_id)
+        dfs.append(pd.read_csv(f'{channel_id}_output.csv', sep=';'))
+    df = pd.concat(dfs)
+    df.to_csv('output.csv', sep=';')
