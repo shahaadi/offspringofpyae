70af1e20d77a99f5bf03dcd6abf99d36134bd1e6
Rohan Dugad <rohand2468@gmail.com>
2023-08-04 05:00:42 -0400

    Parallelized more

--- week4 - e/parallelize_processes.py
+++ week4 - e/parallelize_processes.py
@@ -1,19 +1,28 @@
 import subprocess
 import math
 
-processes = []
+global_start = 0
+global_end = 4283
+global_interval = 500
 
-start = 550
-end = 1110
-interval = 25
-command_len = math.ceil((end - start) / interval)
+run_cnt = math.ceil((global_end - global_start) / global_interval)
 
-command_template = 'python3 spectrogram_production.py --csv-file ncs.csv --start-idx %d --end-idx %d'
+for run in range(run_cnt):
+    start = run * global_interval + global_start
+    end = min((run + 1) * global_interval + global_start, global_end)
 
-for i in range(command_len):
-    start_idx = i * interval + start
-    end_idx = min((i + 1) * interval + start, end)
-    processes.append(subprocess.Popen(command_template % (start_idx, end_idx), shell=True))
+    print('Processing:', start, 'to', end)
 
-for process in processes:
-    process.wait()
+    processes = []
+    interval = 25
+    command_len = math.ceil((end - start) / interval)
+
+    command_template = 'python3 spectrogram_production.py --csv-file outgood.csv --start-idx %d --end-idx %d'
+
+    for i in range(command_len):
+        start_idx = i * interval + start
+        end_idx = min((i + 1) * interval + start, end)
+        processes.append(subprocess.Popen(command_template % (start_idx, end_idx), shell=True))
+
+    for process in processes:
+        process.wait()
